89f3cb385737c938f43d302f74b30b6070d221a5
Eliel <eliel.dourado>
2017-05-16 21:42:06 -0300

    Teste Git;

---
+++ CriptografiaRedes/src/main/java/br/univel/criptografia/Codificar.java
@@ -0,0 +1,15 @@
+package br.univel.criptografia;
+
+public class Codificar {
+
+	public static String criptografar(String valorC) {
+
+		return valorC;
+	}
+
+	public static String descriptografar(String valorD) {
+
+		return valorD;
+	}
+
+}
